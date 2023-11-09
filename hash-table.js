c5529a8ce589e5187d792afcb9803cead71f9c9d
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-11-09 06:22:43 +0300

    End practice

--- hash-table.js
+++ hash-table.js
@@ -32,15 +32,57 @@ class HashTable {
 
   insertNoCollisions(key, value) {
     // Your code here
+    const index = this.hashMod(key)
+    if (this.data[index]) throw new Error('hash collision or same key/value pair already exists!');
+    else {
+      this.data[index] = new KeyValuePair(key, value)
+      this.count++
+    }
   }
 
   insertWithHashCollisions(key, value) {
     // Your code here
+    const index = this.hashMod(key)
+    const newPair = new KeyValuePair(key, value)
+
+    if (this.data[index]) {
+      const oldPairs = this.data[index]
+      newPair.next = oldPairs
+      this.data[index] = newPair
+    } else {
+      this.data[index] = newPair
+    }
+
+    this.count++
   }
 
   insert(key, value) {
-    // Your code here
+    const index = this.hashMod(key)
+    const newPair = new KeyValuePair(key, value)
+    let current = this.data[index]
+  
+    if (!current) {
+      this.data[index] = newPair
+      this.count++
+    } else {
+      let nothingChanged = true
+      while (current) {
+        if (key === current.key) {
+          current.value = value
+          nothingChanged = false
+          break
+        }
+        current = current.next
+      }
+  
+      if (nothingChanged) {
+        newPair.next = this.data[index]
+        this.data[index] = newPair
+        this.count++
+      }
+    }
   }
+  
 
 }
 
